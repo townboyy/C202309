82ed693ee5df98cb93bd4b8b098c295ab4a1482f
townboyy <townboyy@naver.com>
2023-09-20 16:29:07 +0900

    week3.2 조건문

--- chanllenge/week3/choiceif.c
+++ chanllenge/week3/choiceif.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main() {
-	int choice;
+	int choice;  // choice라는 변수 설정
 	printf("1. 파일 저장\n");
 	printf("2. 저장 없이 닫기\n");
 	printf("3. 종료\n");
-	scanf_s("%d", &choice);
+	scanf_s("%d", &choice); //choice를 정수변수로 입력받음
 
-	if (choice == 1) {
+	if (choice == 1) { //choice가 1 이면 파일을~~ 출력
 		printf("파일을 저장합니다.");
 	}
-	else if (choice == 2) {
+	else if (choice == 2) { // choice가 2 이면 저장없이~~ 를 출력
 		printf("저장 없이 닫습니다.");
 
 	}
-	else if (choice == 3) {
+	else if (choice == 3) { //choice가 3 이면 종료합니다를 출력.
 		printf("종료합니다.");
 
 	}
-	else {
+	else {// choice가 123 이 아니면 잘못입력하셨습니다를 출력.
 		printf("잘못 입력하셨습니다.");
 	}
 }
--- chanllenge/week3/zerotwoif.c
+++ chanllenge/week3/zerotwoif.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-	int num = 0;
-	printf("숫자를 입력하시오: ");
-	scanf_s(" %d", &num);
-	if (num == 0) {
+	int num = 0; // num 이라는 변수 설정
+	printf("숫자를 입력하시오: "); 
+	scanf_s(" %d", &num); //num을 정수로 입력받음
+	if (num == 0) { // num가 0이면 0 출력
 		printf("zero");
 	
 	}
-	else if (num == 1) {
+	else if (num == 1) { // num이 1 이면 1 출력
 		printf("one");
 
 	}
-	else if (num == 2) {
+	else if (num == 2) { // num이 2 이면 2 출력
 		printf("two");
 
 	}
-	else {
+	else { // num이 0 1 2 가 아니면 출력 안 함
 		printf("not 0~2");
 	}
 }
