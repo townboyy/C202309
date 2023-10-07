3a0a3ee4b77da0135edb14b7ab51f89080726d50
townboyy <townboyy@naver.com>
2023-10-07 14:46:21 +0900

    week5.함수개념

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -2,7 +2,7 @@
 
 int multif(int value);
 
-void main() { //메인 함수 설정
+int main() { //메인 함수 설정
 	printf("1부터 2까지의 곱은 %d\n", multif(2));
 	printf("1부터 3까지의 곱은 %d\n", multif(3));
 	printf("1부터 5까지의 곱은 %d\n", multif(5));
@@ -10,13 +10,14 @@ void main() { //메인 함수 설정
 }
 int multif(int value) { // i 변수 설정
 	int i;
-	long multif  = 1; //변수 multif선언
+	long multif = 1; //변수 multif선언
 
 	i = 1; //i 1부터 시작
 	while (i <= value) { //i가 value보다 작으면 반복
 
-		multif =  multif  * i;// mulfif는 multif*i로 출력
+		multif = multif * i;// mulfif는 multif*i로 출력
 		i++; //i1씩 증가
 	}
-	return multif ; //multif 반환
+	return multif; //multif 반환
+	return 0;
 }
--- practice/week5/functionHap.c
+++ practice/week5/functionHap.c
@@ -2,7 +2,7 @@
 
 int hapf(int value);
 
-void main() {
+int main() {
 	printf("1부터 10까지의 합은 %d\n", hapf(10));
 	printf("1부터 100까지의 합은 %d\n", hapf(100));
 	printf("1부터 1000까지의 합은 %d\n", hapf(1000));
@@ -11,12 +11,13 @@ void main() {
 int hapf(int value) { //i선언
 	int i;
 	int hap = 0; // hap 선언
-	 
+
 	i = 1;// 1부터
-	while (i <=value) { //i가 value보다 작거나 같으면 반복
-		
-			hap = hap + i;//hap +i로 출력
-			i++;//i 1 씩 증가
+	while (i <= value) { //i가 value보다 작거나 같으면 반복
+
+		hap = hap + i;//hap +i로 출력
+		i++;//i 1 씩 증가
 	}
 	return hap; // hap 출력
+	return 0;
 }
